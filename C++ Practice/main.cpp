05564a59c634a9dd1d5d725b60491435ae6999f8
franka1 <franka1@email.arizona.edu>
2016-12-19 01:34:26 -0700

    Initial Commit

---
+++ C++ Practice/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++ Practice
+//
+//  Created by Alex Frank on 12/19/16.
+//  Copyright © 2016 Alex Frank. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
