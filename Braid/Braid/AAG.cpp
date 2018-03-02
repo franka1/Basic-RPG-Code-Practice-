9e66716f42c29011ed82664f9c1661644d657c65
franka1 <franka1@email.arizona.edu>
2018-03-01 17:32:15 -0700

    Started new project. Implementing Braid Group and Anshel Anshel Goldfeld key exchange.

---
+++ Braid/Braid/AAG.cpp
@@ -0,0 +1,8 @@
+//
+//  AAG.cpp
+//  Braid_AAG
+//
+//  Created by Alex Frank on 2/28/18.
+//
+
+#include "AAG.hpp"
---
+++ Braid/Braid/AAG.hpp
@@ -0,0 +1,13 @@
+//
+//  AAG.hpp
+//  Braid_AAG
+//
+//  Created by Alex Frank on 2/28/18.
+//
+
+#ifndef AAG_hpp
+#define AAG_hpp
+
+#include <stdio.h>
+
+#endif /* AAG_hpp */
